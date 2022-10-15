34847ad409ebb895c87c2d45ecceb0afade6721f
iamstutishah <shah0794@umn.edu>
2022-10-15 16:12:07 -0500

    made the login page and added the sign in with google functionality

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   );
 }
 
+// put sess
 export default MyApp;
