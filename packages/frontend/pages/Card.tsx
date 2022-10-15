35da8e2a6c3f3a7751dc5e70b39fd49c54553b74
Ansh Patel <pate1011@umn.edu>
2022-10-15 14:16:18 -0500

    dashboardRightv2

--- packages/frontend/pages/Card.tsx
+++ packages/frontend/pages/Card.tsx
@@ -15,16 +15,18 @@ export default function Cards() {
       rounded={"md"}
       p={6}
       overflow={"hidden"}
+      _hover={{ boxShadow: "0px 6px 14px rgba(0, 0, 0, 0.2)" }}
       onClick={() => window.open("https://anshpa.tel")}
     >
       <Box h={"285px"} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"top"} />
-      <Stack>
+      <Stack textAlign={"left"}>
         <Text
           color={"gray.500"}
           textTransform={"uppercase"}
           fontWeight={800}
           fontSize={"10px"}
           letterSpacing={1.1}
+          width={"100%"}
         >
           October 15, 2022
         </Text>
@@ -32,10 +34,11 @@ export default function Cards() {
           color={useColorModeValue("gray.700", "white")}
           fontSize={"20px"}
           fontFamily={"body"}
+          width={"100%"}
         >
           Projectile Motion
         </Heading>
-        <Text color={"gray.500"} fontSize={"15px"}>
+        <Text color={"gray.500"} fontSize={"15px"} width={"100%"}>
           Summary
         </Text>
       </Stack>
