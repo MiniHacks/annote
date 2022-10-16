03766bb0902d697af1fa967b2451ffc6a7b7e003
Brandon <brandonnguyenlam@gmail.com>
2022-10-15 22:13:24 -0500

    Landing page 🐢🐢🐢🐢🐢🐢🐢🐢🐢🐢 need to fix margins

--- packages/frontend/pages/landingpage.tsx
+++ packages/frontend/pages/landingpage.tsx
@@ -1,6 +1,14 @@
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import { Box, Button, Heading, HStack, VStack, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  VStack,
+  Image,
+  Center,
+} from "@chakra-ui/react";
 
 import React from "react";
 import PageLayout from "../components/Layout/PageLayout";
@@ -41,48 +49,42 @@ const Home: NextPage = () => {
   console.log(status);
 
   return (
-    <PageLayout title={"geese, by minihacks"}>
-      <Box backgroundColor={"yellow.50"} minHeight={"100vh"}>
-        <NavBar />
-        <Box>
-          <VStack minHeight={"200px"} justifyContent={"center"}>
-            <Heading
-              style={{
-                textAlign: "center",
-              }}
-            >
-              What a great day to take annote.
-            </Heading>
-          </VStack>
-        </Box>
-        <Box
-          paddingX={"5%"}
-          paddingY={"5%"}
-          textAlign={"center"}
-          boxSize={"sm"}
-        >
-          <Image
-            htmlHeight={"100px"}
-            htmlWidth={"200px"}
-            src={
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/2560px-Download_on_the_App_Store_Badge.svg.png"
-            }
-            alt={"Download on the app store."}
-          />
-        </Box>
-        <Box>
-          <VStack minHeight={"500px"} justifyContent={"center"}>
-            <Heading
-              style={{
-                textAlign: "center",
-              }}
-            >
-              What a great day to take annote.
-            </Heading>
-          </VStack>
-        </Box>
+    <Box backgroundColor={"yellow.50"} minHeight={"100vh"}>
+      <NavBar />
+      <Box>
+        <VStack minHeight={"200px"} justifyContent={"center"}>
+          <Heading
+            style={{
+              textAlign: "center",
+            }}
+          >
+            What a great day to take annote.
+          </Heading>
+          <Box>
+            Annote. Live transcription and summarization in your all-in-one
+            digital notebook.
+          </Box>
+        </VStack>
       </Box>
-    </PageLayout>
+      <Center>
+        <Image
+          htmlHeight={"60%"}
+          htmlWidth={"60%"}
+          src={"https://i.imgur.com/V27w0oH.png"}
+          alt={"Download on the app store."}
+        />
+      </Center>
+      <Center>
+        <Image
+          htmlHeight={"100px"}
+          htmlWidth={"200px"}
+          src={
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/2560px-Download_on_the_App_Store_Badge.svg.png"
+          }
+          alt={"Download on the app store."}
+        />
+      </Center>
+    </Box>
   );
 };
 
